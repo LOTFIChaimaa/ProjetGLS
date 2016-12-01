baa4df4fa7ff2d44b92a296e88d2b31bb5328b18
Jayjader <njay47@gmail.com>
2016-12-01 19:41:49 +0100

    Added Deposable, Transmission, Trouvable attributes.

--- Game/src/game/Transmission.java
+++ Game/src/game/Transmission.java
@@ -1,5 +1,12 @@
 package game;
 
+import java.util.List;
+
 public class Transmission {
+
+    private List<Objet> objetsTransmis;
+    private List<Objet> objetsConsommes;
+    private List<Connaissance> connaissances;
+    private List<Condition> conditions;
 
 }
--- Game/src/game/Trouvable.java
+++ Game/src/game/Trouvable.java
@@ -1,5 +1,14 @@
 package game;
 
+import java.util.List;
+
 public class Trouvable {
+
+    // concerne peut etre un Objet, une Connaissance ou une Personne
+    private Object concerne;
+    private boolean visible;
+    private boolean actif;
+    private List<Condition> conditionsVisibilite;
+    private List<Condition> conditionsActivation;
 
 }
