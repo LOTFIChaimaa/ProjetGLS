67fa4891bb2dda20d474073a77db29171a6467a9
Jayjader <njay47@gmail.com>
2016-12-02 00:13:23 +0100

    tabs -> spaces

--- Game/src/game/Chemin.java
+++ Game/src/game/Chemin.java
@@ -3,14 +3,14 @@ package game;
 import java.util.List;
 
 public class Chemin {
-	
-	private String name; // Le nom du chemin
-	private List<Description> descriptions; // Les descriptions possibles décrivant le chemin
-	private Lieu lieu1; // un des lieux relies par le chemin
-	private Lieu lieu2; // l'autre lieu relie par le chemin
-	private boolean ouvert; // Indique si le chemin est ouvert
-	private boolean visible; // Indique si le chemin est visible
-	private List<Condition> conditionsVisibilite;
-	private List<Condition> conditionsOuverture;
+
+    private String name; // Le nom du chemin
+    private List<Description> descriptions; // Les descriptions possibles décrivant le chemin
+    private Lieu lieu1; // un des lieux relies par le chemin
+    private Lieu lieu2; // l'autre lieu relie par le chemin
+    private boolean ouvert; // Indique si le chemin est ouvert
+    private boolean visible; // Indique si le chemin est visible
+    private List<Condition> conditionsVisibilite;
+    private List<Condition> conditionsOuverture;
 
 }
