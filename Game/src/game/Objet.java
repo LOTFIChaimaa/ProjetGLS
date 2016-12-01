f14bab3bacb8035939c5e277687079fb668ad2f2
Jayjader <njay47@gmail.com>
2016-12-02 00:06:36 +0100

    Tabs -> spaces

--- Game/src/game/Objet.java
+++ Game/src/game/Objet.java
@@ -3,33 +3,34 @@ package game;
 import java.util.List;
 
 public class Objet {
-	
-	private String name; // Le nom de l'objet
-	private List<Description> descriptions; // Les descriptions possibles de l'objets
-	private List<Transformation> transformations; // Les transformations possibles de l'objets
-	private int taille; // La taille de l'objet
-	
-	public Objet(String name, List<Description> descriptions, List<Transformation> transformations, int taille) {
-		this.name = name;
-		this.descriptions = descriptions;
-		this.transformations = transformations;
-		this.taille = taille;
-	}
 
-	public String getName() {
-		return name;
-	}
+    private String name; // Le nom de l'objet
+    private List<Description> descriptions; // Les descriptions possibles de l'objet
+    private List<Transformation> transformations; // Les transformations possibles de l'objet
+    private int taille; // La taille de l'objet
 
-	public List<Description> getDescriptions() {
-		return descriptions;
-	}
+    public Objet(String name, List<Description> descriptions,
+            List<Transformation> transformations, int taille) {
+        this.name = name;
+        this.descriptions = descriptions;
+        this.transformations = transformations;
+        this.taille = taille;
+    }
 
-	public List<Transformation> getTransformations() {
-		return transformations;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getTaille() {
-		return taille;
-	}
-	
+    public List<Description> getDescriptions() {
+        return descriptions;
+    }
+
+    public List<Transformation> getTransformations() {
+        return transformations;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
 }
