738a1897394914cc3f8cd73e8a20e4fabe56b400
Jayjader <njay47@gmail.com>
2016-12-02 11:36:16 +0100

    Cleanup
    tabs -> spaces
    removed trailing whitespace.

--- Game/src/game/Interaction.java
+++ Game/src/game/Interaction.java
@@ -1,24 +1,22 @@
 package game;
 
-
 import java.util.List;
 
 public class Interaction {
-	
-	private List<Transmission> transmissions; // Les transmissions possibles de l'intéraction
-	private List<Choix> choix; // Tous les choix possibles de l'intéraction
-	
-	public Interaction(List<Transmission> transmissions, List<Choix> choix) {
-		this.transmissions = transmissions;
-		this.choix = choix;
-	}
 
-	public List<Transmission> getTransmissions() {
-		return transmissions;
-	}
+    private List<Transmission> transmissions; // Les transmissions possibles de l'intéraction
+    private List<Choix> choix; // Tous les choix possibles de l'intéraction
 
-	public List<Choix> getChoix() {
-		return choix;
-	}
+    public Interaction(List<Transmission> transmissions, List<Choix> choix) {
+        this.transmissions = transmissions;
+        this.choix = choix;
+    }
 
+    public List<Transmission> getTransmissions() {
+        return transmissions;
+    }
+
+    public List<Choix> getChoix() {
+        return choix;
+    }
 }
