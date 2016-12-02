a18831dcc8da8e17f6979178971e25100e4a8369
Jayjader <njay47@gmail.com>
2016-12-02 11:21:27 +0100

    Added constructor

--- Game/src/game/Transmission.java
+++ Game/src/game/Transmission.java
@@ -9,4 +9,11 @@ public class Transmission {
     private List<Connaissance> connaissances;
     private List<Condition> conditions;
 
+    public Transmission(List<Objet> transmis, List<Objet> consommes,
+            List<Connaissance> connaissances, List<Condition> conditions) {
+        this.objetsTransmis = transmis;
+        this.objetsConsommes = consommes;
+        this.connaissances = connaissances;
+        this.conditions = conditions;
+    }
 }
