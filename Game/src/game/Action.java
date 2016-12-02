7ff6d6cea09657d9e457cdbbbd9f53040794aa1d
Jayjader <njay47@gmail.com>
2016-12-02 11:33:23 +0100

    Cleanup
    tabs -> spaces
    removed trailing whitespaces.

--- Game/src/game/Action.java
+++ Game/src/game/Action.java
@@ -3,29 +3,26 @@ package game;
 import java.util.List;
 
 public class Action {
-	
-	List<Choix> choixNecessaires; // Les choix précédents de l"explorateur dont dépendent l'action
-	List<Condition> conditions; // Les conditions de l'action
-	List<Transmission> transmissions; // Les transmissions possibles lors de l'action
 
-	public Action(List<Choix> choixNecessaires, List<Condition> conditions, List<Transmission> transmissions) {
-		this.choixNecessaires = choixNecessaires;
-		this.conditions = conditions;
-		this.transmissions = transmissions;
-	}
+    List<Choix> choixNecessaires; // Les choix précédents de l"explorateur dont dépendent l'action
+    List<Condition> conditions; // Les conditions de l'action
+    List<Transmission> transmissions; // Les transmissions possibles lors de l'action
 
-	public List<Choix> getChoixNecessaires() {
-		return choixNecessaires;
-	}
+    public Action(List<Choix> choixNecessaires, List<Condition> conditions, List<Transmission> transmissions) {
+        this.choixNecessaires = choixNecessaires;
+        this.conditions = conditions;
+        this.transmissions = transmissions;
+    }
 
-	public List<Condition> getConditions() {
-		return conditions;
-	}
+    public List<Choix> getChoixNecessaires() {
+        return choixNecessaires;
+    }
 
-	public List<Transmission> getTransmissions() {
-		return transmissions;
-	}
+    public List<Condition> getConditions() {
+        return conditions;
+    }
 
-	
-	
+    public List<Transmission> getTransmissions() {
+        return transmissions;
+    }
 }
