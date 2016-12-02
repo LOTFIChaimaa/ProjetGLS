84478c639fd8b9464d6491cfa08f12520f1fc6c9
Jayjader <njay47@gmail.com>
2016-12-02 03:01:09 +0100

    Things (Deposable) can now check if they can be deposited.

--- Game/src/game/Deposable.java
+++ Game/src/game/Deposable.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 public class Deposable {
 
-    private Objet concerne;
+    private Objet objet;
     private List<Condition> conditions;
 
+    /** Checks if the object can be deposited.
+     * @param player Explorateur
+     * @return boolean
+     */
+    public boolean check(Explorateur player) {
+        for (Condition c : this.conditions) {
+            if (!player.check(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
