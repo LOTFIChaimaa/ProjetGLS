0980c2752814f5fd3900e734b280815085b82fdc
Jayjader <njay47@gmail.com>
2016-12-02 01:10:32 +0100

    Discoverables (Trouvable) can now check if they are visible and/or activated.
    Removed useless attributes.

--- Game/src/game/Trouvable.java
+++ Game/src/game/Trouvable.java
@@ -6,9 +6,34 @@ public class Trouvable {
 
     // l'entite contenue peut etre un Objet, une Connaissance ou une Personne
     private Object entite;
-    private boolean visible;
-    private boolean actif;
     private List<Condition> conditionsVisibilite;
     private List<Condition> conditionsActivation;
 
+    /** Check if this discoverable is visible by the player.
+     * @param joueur Explorateur
+     * @return boolean
+     */
+    public boolean estVisible(Explorateur player) {
+        for (Condition c : this.conditionsVisibilite) {
+            if (!player.check(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /** Check if this discoverable is visible by the player.
+     * @param joueur Explorateur
+     * @return boolean
+     */
+    public boolean estActif(Explorateur player) {
+        for (Condition c : this.conditionsActivation) {
+            if (!player.check(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
