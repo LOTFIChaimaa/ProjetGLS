abdcd87e37c815200325fac743378a3455154587
Jayjader <njay47@gmail.com>
2016-12-02 03:36:05 +0100

    tabs -> spaces

--- Game/src/game/Lieu.java
+++ Game/src/game/Lieu.java
@@ -4,35 +4,35 @@ import java.util.List;
 
 public class Lieu {
 
-	private String name; // Le nom du lieu
-	private List<Trouvable> trouvables; // Les objets, personnes ou connaissance trouvables dans le lieu
-	private List<Deposable> deposables; // Les objets déposés dans le lieu
-	private List<Chemin> cheminsPossibles; // Les chemins possibles reliés au lieu
-	private List<Chemin> cheminsObligatoires; // Les chemins obligatoires reliés au lieu
+    private String name; // Le nom du lieu
+    private List<Trouvable> trouvables; // Les objets, personnes ou connaissance trouvables dans le lieu
+    private List<Deposable> deposables; // Les objets déposés dans le lieu
+    private List<Chemin> cheminsPossibles; // Les chemins possibles reliés au lieu
+    private List<Chemin> cheminsObligatoires; // Les chemins obligatoires reliés au lieu
 
-	public Lieu(String name, List<Trouvable> trouvables, List<Deposable> deposables, List<Chemin> cheminsPossibles,
-			List<Chemin> cheminsObligatoires) {
-		this.name = name;
-		this.trouvables = trouvables;
-		this.deposables = deposables;
-		this.cheminsPossibles = cheminsPossibles;
-		this.cheminsObligatoires = cheminsObligatoires;
-	}
-	public String getName() {
-		return name;
-	}
-	public List<Trouvable> getTrouvables() {
-		return trouvables;
-	}
-	public List<Deposable> getDeposables() {
-		return deposables;
-	}
-	public List<Chemin> getCheminsPossibles() {
-		return cheminsPossibles;
-	}
-	public List<Chemin> getCheminsObligatoires() {
-		return cheminsObligatoires;
-	}
-	
-	
+    public Lieu(String name, List<Trouvable> trouvables, List<Deposable> deposables, List<Chemin> cheminsPossibles,
+            List<Chemin> cheminsObligatoires) {
+        this.name = name;
+        this.trouvables = trouvables;
+        this.deposables = deposables;
+        this.cheminsPossibles = cheminsPossibles;
+        this.cheminsObligatoires = cheminsObligatoires;
+    }
+    public String getName() {
+        return name;
+    }
+    public List<Trouvable> getTrouvables() {
+        return trouvables;
+    }
+    public List<Deposable> getDeposables() {
+        return deposables;
+    }
+    public List<Chemin> getCheminsPossibles() {
+        return cheminsPossibles;
+    }
+    public List<Chemin> getCheminsObligatoires() {
+        return cheminsObligatoires;
+    }
+
+
 }
