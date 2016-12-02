3f40ec3df95e9cede5d09f1bd175bfd77a310429
Jayjader <njay47@gmail.com>
2016-12-02 18:05:43 +0100

    Formatting
    tabs -> spaces
    more concise parameter names

--- Game/src/game/Jeu.java
+++ Game/src/game/Jeu.java
@@ -4,80 +4,73 @@ import java.util.List;
 
 /** Classe définissant le jeu
  * Contient la plus part des éléments qui seront contenues dans le jeu
- * 
+ *
  */
 public class Jeu {
-	
-	private String name; // Le nom du jeu
-	private List<Lieu> lieux; // Tous les lieux du jeu
-	private List<Chemin> chemins; // Tous les chemins du lieu
-	private Explorateur joueur; // Le joueur
-	private List<Difficulte> difficultes; // Toutes les difficultés possibles
-	private List<Connaissance> connaissances; // Toutes les connaissances du jeu
-	private List<Objet> objets; // Tous les objets du jeu
-	private List<Personne>  personnes; // Toutes les personnes du jeu
-	private Lieu lieuDepart; // Le lieu de départ
-	private Lieu lieuArrivee; // Le lieu d'arrivée
-	
-	public Jeu(String name, List<Lieu> lieux, List<Chemin> chemins, Explorateur joueur, List<Difficulte> difficultes,
-			List<Connaissance> connaissances, List<Objet> objets, List<Personne> personnes, Lieu lieuDepart,
-			Lieu lieuArrivee) {
-		this.name = name;
-		this.lieux = lieux;
-		this.chemins = chemins;
-		this.joueur = joueur;
-		this.difficultes = difficultes;
-		this.connaissances = connaissances;
-		this.objets = objets;
-		this.personnes = personnes;
-		this.lieuDepart = lieuDepart;
-		this.lieuArrivee = lieuArrivee;
-	}
 
-	public String getName() {
-		return name;
-	}
+    private String name; // Le nom du jeu
+    private List<Lieu> lieux; // Tous les lieux du jeu
+    private List<Chemin> chemins; // Tous les chemins du lieu
+    private Explorateur joueur; // Le joueur
+    private List<Difficulte> difficultes; // Toutes les difficultés possibles
+    private List<Connaissance> connaissances; // Toutes les connaissances du jeu
+    private List<Objet> objets; // Tous les objets du jeu
+    private List<Personne>  personnes; // Toutes les personnes du jeu
+    private Lieu lieuDepart; // Le lieu de départ
+    private Lieu lieuArrivee; // Le lieu d'arrivée
 
-	public List<Lieu> getLieux() {
-		return lieux;
-	}
+    public Jeu(String name, List<Lieu> l, List<Chemin> ch, Explorateur player,
+            List<Difficulte> d, List<Connaissance> cn, List<Objet> o,
+            List<Personne> p, Lieu ld, Lieu la) {
+        this.name = name;
+        this.lieux = l;
+        this.chemins = ch;
+        this.joueur = player;
+        this.difficultes = d;
+        this.connaissances = cn;
+        this.objets = o;
+        this.personnes = p;
+        this.lieuDepart = ld;
+        this.lieuArrivee = la;
+    }
 
-	public List<Chemin> getChemins() {
-		return chemins;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Explorateur getJoueur() {
-		return joueur;
-	}
+    public List<Lieu> getLieux() {
+        return lieux;
+    }
 
-	public List<Difficulte> getDifficultes() {
-		return difficultes;
-	}
+    public List<Chemin> getChemins() {
+        return chemins;
+    }
 
-	public List<Connaissance> getConnaissances() {
-		return connaissances;
-	}
+    public Explorateur getJoueur() {
+        return joueur;
+    }
 
-	public List<Objet> getObjets() {
-		return objets;
-	}
+    public List<Difficulte> getDifficultes() {
+        return difficultes;
+    }
 
-	public List<Personne> getPersonnes() {
-		return personnes;
-	}
+    public List<Connaissance> getConnaissances() {
+        return connaissances;
+    }
 
-	public Lieu getLieuDepart() {
-		return lieuDepart;
-	}
+    public List<Objet> getObjets() {
+        return objets;
+    }
 
-	public Lieu getLieuArrivee() {
-		return lieuArrivee;
-	}
-	
-	
-	
+    public List<Personne> getPersonnes() {
+        return personnes;
+    }
 
+    public Lieu getLieuDepart() {
+        return lieuDepart;
+    }
 
-	
-
+    public Lieu getLieuArrivee() {
+        return lieuArrivee;
+    }
 }
