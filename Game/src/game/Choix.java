5d2d1c7a9ab5c8c80f2761fb41e972608d742310
Jayjader <njay47@gmail.com>
2016-12-02 12:12:50 +0100

    tabs -> spaces

--- Game/src/game/Choix.java
+++ Game/src/game/Choix.java
@@ -3,39 +3,38 @@ package game;
 import java.util.List;
 
 public class Choix {
-	
-	private String name; // Le nom du choix
-	private List<Action> actions; // Les actions du choix
-	private boolean estDebut; // indique si c'est un choix de début
-	private boolean estFin; // indique si c'est un choix de fin
-	private List<Condition> conditions; // Les conditions du choix
-	
-	public Choix(String name, List<Action> actions, boolean estDebut, boolean estFin, List<Condition> conditions) {
-		this.name = name;
-		this.actions = actions;
-		this.estDebut = estDebut;
-		this.estFin = estFin;
-		this.conditions = conditions;
-	}
 
-	public String getName() {
-		return name;
-	}
+    private String name; // Le nom du choix
+    private List<Action> actions; // Les actions du choix
+    private boolean estDebut; // indique si c'est un choix de début
+    private boolean estFin; // indique si c'est un choix de fin
+    private List<Condition> conditions; // Les conditions du choix
 
-	public List<Action> getActions() {
-		return actions;
-	}
+    public Choix(String name, List<Action> actions, boolean estDebut, boolean estFin, List<Condition> conditions) {
+        this.name = name;
+        this.actions = actions;
+        this.estDebut = estDebut;
+        this.estFin = estFin;
+        this.conditions = conditions;
+    }
 
-	public boolean isEstDebut() {
-		return estDebut;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public boolean isEstFin() {
-		return estFin;
-	}
+    public List<Action> getActions() {
+        return actions;
+    }
 
-	public List<Condition> getConditions() {
-		return conditions;
-	}
+    public boolean isEstDebut() {
+        return estDebut;
+    }
 
+    public boolean isEstFin() {
+        return estFin;
+    }
+
+    public List<Condition> getConditions() {
+        return conditions;
+    }
 }
